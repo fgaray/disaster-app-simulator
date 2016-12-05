1ee2879cf263ce6f2760b11e520f4537a0dda203
Benja <benjamin.ruiztagle@usach.cl>
2016-12-05 15:37:01 -0300

    precision trainer

--- src/pe/PETrainer.cpp
+++ src/pe/PETrainer.cpp
@@ -1,6 +1,7 @@
 #include "PETrainer.hpp"
 
-
+static int current_precision = 0;
+static int count_tag = 0;
 
 
 double  PETrainer::getCostTime(){
@@ -9,6 +10,13 @@ double  PETrainer::getCostTime(){
 }
 
 std::vector<std::tuple<PEName, MessagePE>> PETrainer::nextPE(MessagePE message){
+	if (message.tag){
+		count_tag++;
+		if (count_tag == 50){
+			count_tag = 0;
+			current_precision = current_precision + 0.1;
+		}
+	}
   //envía el mismo mensaje sin modificar a PEAssembler
 	return {std::make_tuple(PEName::PEModel, message)};
 }
